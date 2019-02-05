1e8b7c856a1c4321dd60181375b9c9fefc65071d
Matthew Harrilal <matthewharrilal@gmail.com>
2019-02-05 09:52:41 -0800

    Count sort is working however have to refactor to not use the output array

--- Code/sorting_integer.py
+++ Code/sorting_integer.py
@@ -1,17 +1,33 @@
 #!python
 
 
-def counting_sort(numbers):
-    """Sort given numbers (integers) by counting occurrences of each number,
-    then looping over counts and copying that many numbers into output list.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Find range of given numbers (minimum and maximum integer values)
-    # TODO: Create list of counts with a slot for each number in input range
-    # TODO: Loop over given numbers and increment each number's count
-    # TODO: Loop over counts and append that many numbers into output list
-    # FIXME: Improve this to mutate input instead of creating new output list
 
+def count_sort(numbers):
+  minimum, maximum = min(numbers), max(numbers)
+  count_array = [0] * ((maximum + 1) - minimum)
+  offset = minimum
+  output = [0] * len(numbers)
+
+
+  for index in range(0, len(numbers)):
+    current_element = numbers[index]
+    if count_array[current_element - offset] > 0: # Meaning that we've seen an occurence
+      count_array[current_element - offset] += 1
+    else:
+      count_array[current_element - offset] = 1 # Meaning we havent
+
+  for index in range(0, len(count_array) - 1):
+    next_index = index + 1
+
+    count_array[next_index] += count_array[index]
+
+  for index,num in enumerate(numbers):
+    # print(num, num - offset, count_array)
+    output[count_array[num - offset] - 1] = num # Have to subtract by 1 to represent index
+    count_array[num - offset] -= 1 # Decrement the number of occurences we accounted for by 1
+
+  return output
+    
 
 def bucket_sort(numbers, num_buckets=10):
     """Sort given numbers by distributing into buckets representing subranges,
