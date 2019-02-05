2e3f1821d23a2521a9b6af129c9062333df77d5d
Matthew Harrilal <matthewharrilal@gmail.com>
2019-02-05 11:24:36 -0800

    Finished count sort and optimized to alter input in place ... took awhile

--- Code/sorting_integer.py
+++ Code/sorting_integer.py
@@ -1,32 +1,36 @@
 #!python
 
 
+def counting_sort(numbers):
+    """Sort given numbers (integers) by counting occurrences of each number,
+    then looping over counts and copying that many numbers into output list.
+    TODO: Running time: ??? Why and under what conditions?
+    TODO: Memory usage: ??? Why and under what conditions?"""
+    # TODO: Find range of given numbers (minimum and maximum integer values)
+    # TODO: Create list of counts with a slot for each number in input range
+    # TODO: Loop over given numbers and increment each number's count
+    # TODO: Loop over counts and append that many numbers into output list
+    # FIXME: Improve this to mutate input instead of creating new output list
+    minimum, maximum = min(numbers), max(numbers)
+    count_array = [0] * ((maximum + 1) - minimum)
+    offset = minimum
+    output = [0] * len(numbers)
 
-def count_sort(numbers):
-  minimum, maximum = min(numbers), max(numbers)
-  count_array = [0] * ((maximum + 1) - minimum)
-  offset = minimum
-  output = [0] * len(numbers)
 
+    for index in range(0, len(numbers)):
+        current_element = numbers[index]
+        # if count_array[current_element - offset] > 0: # Meaning that we've seen an occurence
+        count_array[current_element - offset] += 1
+    
+    i = 0
+    for num in range(len(count_array)): # Represents the index which is the value in input
+        for occurence in range(count_array[num]): # Iterate over how many times you see that specific element
+            numbers[i] = num + offset
+            i  +=  1
+    
+    
 
-  for index in range(0, len(numbers)):
-    current_element = numbers[index]
-    if count_array[current_element - offset] > 0: # Meaning that we've seen an occurence
-      count_array[current_element - offset] += 1
-    else:
-      count_array[current_element - offset] = 1 # Meaning we havent
-
-  for index in range(0, len(count_array) - 1):
-    next_index = index + 1
-
-    count_array[next_index] += count_array[index]
-
-  for index,num in enumerate(numbers):
-    # print(num, num - offset, count_array)
-    output[count_array[num - offset] - 1] = num # Have to subtract by 1 to represent index
-    count_array[num - offset] -= 1 # Decrement the number of occurences we accounted for by 1
-
-  return output
+  return numbers
     
 
 def bucket_sort(numbers, num_buckets=10):
