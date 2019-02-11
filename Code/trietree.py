ebfddab19543fcac55409849b4cee82a8dce2726
Matthew Harrilal <matthewharrilal@gmail.com>
2019-02-11 11:18:29 -0800

    Finished search and find functions for the Trie Tree

--- Code/trietree.py
+++ Code/trietree.py
@@ -3,12 +3,14 @@ class TrieTrieeNode(object):
         # What does a node in a Trie Tree consist of an array references, each node contains data, and a flag representing that the end of a sequence exists
         self.flag = False
         self.data = data
-        self.references = [] # Using an array to represent the node's references ... an array of trie tree node objects
-
+        # Since we know that every node has references containing up to 26 null node values
+        self.references = [None] * 26
 
 
 class TrieTree(object):
     def __init__(self, vocabulary=None):
+        # root node will always be an empty string
+        self.root = TrieTrieeNode(" ")
         # User will pass in an array of vocabulary words ... for every word we want to insert it into our tree
         if len(vocabulary) > 0:
             for word in vocabulary:
@@ -16,33 +18,61 @@ class TrieTree(object):
 
     def find(self, word):
         # Find a node in the given trie tree given the name of the word
-        pass
+        root_node = self.root
+
+        for index in range(0, len(word)):
+
+            current_letter = word[index]
+            current_node = TrieTrieeNode(current_letter)
+            # print(root_node.references)
+            # print("")
+            position = self.__alphabet_index_helper(current_letter)
+
+            # print(root_node.data,root_node.references, root_node.references[position].data)
+
+            # If at the position there is no value meaning that there is no more pathway
+            if root_node.references[position] is None:
+                print("FOUND NO MORE PATHWAY", current_letter)
+                return False
+            # print(current_node.references)
+            root_node = root_node.references[position]
+
+        if root_node.flag == False:  # If it was a search miss meaning we found it but the flag wasn't marked
+            return False
+        return True
 
     def __alphabet_index_helper(self, letter):
         alphabet = "abcdefghijklmnopqrstuvwxyz"
 
         return alphabet.index(letter)
-    
 
     def insert(self, word):
         # Insert a given word into the Trie Tree
 
-        # Our root node is always an empty string to represent that any word can be inserted
-        empty_string = ""
-        root_node = TrieTrieeNode(empty_string)
+        # Whats auto updating our root node
 
-        root_node.references = [None] * 26 # Allocating the references
+        # Why do we we need recursion because cant we update every iteration the node to be the previous node
 
-        # Now that we have the node's references now as we iterate over each letter in the string as the iterations go deeper
-        
-        
+        # Would we lose the root node if we change the value ... not if we store it
+
+        root_node = self.root
+
         for index in range(0, len(word)):
             # Getting each letter in the word
             current_letter = word[index]
             current_node = TrieTrieeNode(current_letter)
 
+            # What if a pathway already exists?
+            position = self.__alphabet_index_helper(current_letter)
+            root_node.references[position] = current_node
+            # print(root_node.data,root_node.references, current_node.data)
 
-    
+            root_node = root_node.references[position]  # Go one level deeper
+            # print("")
+
+        root_node.flag = True  # To mark that the end of sequence occured here
+        return root_node  # Stub
+
     def delete(self, word):
         # Delete a given node from the Trie Tree
         pass
